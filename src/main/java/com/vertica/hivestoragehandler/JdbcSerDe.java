ac8f7411f9ace4bffc34f7f97e6a9ccfebdaf6b2
bryanherger <bryanherger@gmail.com>
2015-10-12 15:11:36 -0400

    refactor using myui branch

--- src/main/java/com/vertica/hivestoragehandler/JdbcSerDe.java
+++ src/main/java/com/vertica/hivestoragehandler/JdbcSerDe.java
@@ -1,4 +1,19 @@
-package com.vertica.hadoop;
+/*
+ * Copyright 2013-2015 Makoto YUI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vertica.hivestoragehandler;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,8 +35,8 @@ import org.apache.hadoop.io.Writable;
 /**
  * Serialize/Deserialize a tuple.
  */
-public class VerticaSerDe implements SerDe {
-    private static final Log LOG = LogFactory.getLog(VerticaSerDe.class);
+public class JdbcSerDe implements SerDe {
+    private static final Log LOG = LogFactory.getLog(JdbcSerDe.class);
 
     private DbRecordWritable cachedWritable;
 
@@ -119,6 +134,7 @@ public class VerticaSerDe implements SerDe {
 
     @Override
     public SerDeStats getSerDeStats() {
+        // TODO Auto-generated method stub
         return null;
     }
 }
